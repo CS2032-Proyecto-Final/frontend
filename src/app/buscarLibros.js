721901fb431bee9ca91c9579ba3a89af10b5235f
Gino Daza <gino.daza@utec.edu.pe>
2024-11-09 12:32:18 -0500

    prueba de paginacion y diseño basico

---
+++ src/app/buscarLibros.js
@@ -0,0 +1,14 @@
+// src/app/buscarLibros.js
+export const fetchBooks = async (page, tenant_id = "tenant1") => {
+    const url = `https://p3q0o151ag.execute-api.us-east-1.amazonaws.com/dev/books?tenant_id=${tenant_id}&page=${page}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching books:', error);
+      return [];
+    }
+  };
+  
