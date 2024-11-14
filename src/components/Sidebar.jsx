28f607c4a5953d6da8f5928f09f6c2869bbd292d
Gino Daza <gino.daza@utec.edu.pe>
2024-11-14 01:02:48 -0500

    iconos añadidos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,34 @@
 // src/components/Sidebar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTenant } from '../context/TenantContex';
+import {
+  FaHome,
+  FaSearch,
+  FaMapMarkerAlt,
+  FaCalendarCheck,
+  FaHeart,
+  FaCog
+} from 'react-icons/fa';
 import './../css/Sidebar.css';
 
 function Sidebar() {
+  const { tenantInfo } = useTenant();
+  const logoUrl = tenantInfo?.logo_url || '/default-logo.png'; // Ruta predeterminada si no hay logo
+
   return (
     <nav className="sidebar">
+      <div className="sidebar-logo">
+        <img src={logoUrl} alt="Tenant Logo" className="tenant-logo" />
+      </div>
       <h2>Bibliokuna</h2>
       <ul>
-        <li><Link to="/inicio">Inicio</Link></li>
-        <li><Link to="/buscar-libros">Buscar Libros</Link></li>
-        <li><Link to="/ambientes">Ambientes</Link></li>
-        <li><Link to="/mis-reservas">Mis reservas</Link></li>
-        <li><Link to="/mis-favoritos">Mis Favoritos</Link></li>
-        <li><Link to="/configuracion">Configuración</Link></li>
+        <li><Link to="/inicio"><FaHome /> Inicio</Link></li>
+        <li><Link to="/buscar-libros"><FaSearch /> Buscar Libros</Link></li>
+        <li><Link to="/ambientes"><FaMapMarkerAlt /> Ambientes</Link></li>
+        <li><Link to="/mis-reservas"><FaCalendarCheck /> Mis reservas</Link></li>
+        <li><Link to="/mis-favoritos"><FaHeart /> Mis Favoritos</Link></li>
+        <li><Link to="/configuracion"><FaCog /> Configuración</Link></li>
       </ul>
     </nav>
   );
